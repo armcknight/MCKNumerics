3076e94f8dcec809e61f0da35f48a3ba5f54108c
Andrew McKnight <amcknight2718@gmail.com>
2014-05-14 21:51:08 -0400

    added project and classes

---
+++ MCKNumerics/MCKNumerics/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MCKNumerics
+//
+//  Created by Andrew McKnight on 5/14/14.
+//  Copyright (c) 2014 Andrew McKnight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MCKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MCKAppDelegate class]));
+    }
+}
