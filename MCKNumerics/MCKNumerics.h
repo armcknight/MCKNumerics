7553a565086ebf54651aa03af1f6e4f2530f549b
Andrew McKnight <amcknight@twitter.com>
2015-12-26 22:17:41 -0500

    use foundation for cross platform compatibility

--- MCKNumerics/MCKNumerics.h
+++ MCKNumerics/MCKNumerics.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 AMProductions. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MCKNumerics.
 FOUNDATION_EXPORT double MCKNumericsVersionNumber;
